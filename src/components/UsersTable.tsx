76f6a243d3de07d0850a0cbdf58a596731dceb02
Agustin Gabrielli <agabrielli@fi.uba.ar>
2024-08-05 22:37:52 -0300

    Fix tailwindcss not working + improved table

--- src/components/UsersTable.tsx
+++ src/components/UsersTable.tsx
@@ -7,30 +7,34 @@ interface UsersTableProps {
 
 const UsersTable: React.FC<UsersTableProps> = ({ users }) => {
   return (
-    <table className="min-w-full divide-y divide-gray-200">
-      <thead>
-        <tr>
-          <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">FirebaseUID</th>
-          <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">First Name</th>
-          <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Last Name</th>
-          <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Gender</th>
-          <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
-          <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">User Type</th>
-        </tr>
-      </thead>
-      <tbody className="bg-white divide-y divide-gray-200">
-        {users.map((user) => (
-          <tr key={user.firebaseUID}>
-            <td className="px-6 py-4 whitespace-nowrap">{user.firebaseUID}</td>
-            <td className="px-6 py-4 whitespace-nowrap">{user.firstName}</td>
-            <td className="px-6 py-4 whitespace-nowrap">{user.lastName}</td>
-            <td className="px-6 py-4 whitespace-nowrap">{user.gender}</td>
-            <td className="px-6 py-4 whitespace-nowrap">{user.email}</td>
-            <td className="px-6 py-4 whitespace-nowrap">{user.userType}</td>
+    <div className="relative overflow-x-auto">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">Firebase UID</th>
+            <th scope="col" className="px-6 py-3">User Type</th>
+            <th scope="col" className="px-6 py-3">First Name</th>
+            <th scope="col" className="px-6 py-3">Last Name</th>
+            <th scope="col" className="px-6 py-3">Gender</th>
+            <th scope="col" className="px-6 py-3">Email</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.firebaseUID} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {user.firebaseUID}
+              </th>
+              <td className="px-6 py-4">{user.userType}</td>
+              <td className="px-6 py-4">{user.firstName}</td>
+              <td className="px-6 py-4">{user.lastName}</td>
+              <td className="px-6 py-4">{user.gender}</td>
+              <td className="px-6 py-4">{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
