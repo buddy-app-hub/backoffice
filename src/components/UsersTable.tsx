e815bf1874dce63f7ac3dfe38f763a3d1aa5a447
Agustin Gabrielli <66754175+agus-gabrielli@users.noreply.github.com>
2024-08-05 22:43:24 -0300

    Budd 102 skeleton frontend backoffice (#1)
    * Skeleton next js app
    
    * First table version showing users
    
    * Fix tailwindcss not working + improved table
    
    * Names in spanish

---
+++ src/components/UsersTable.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { User } from '../types/User';
+
+interface UsersTableProps {
+  users: User[];
+}
+
+const UsersTable: React.FC<UsersTableProps> = ({ users }) => {
+  return (
+    <div className="relative overflow-x-auto">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">Firebase UID</th>
+            <th scope="col" className="px-6 py-3">Tipo</th>
+            <th scope="col" className="px-6 py-3">Nombre</th>
+            <th scope="col" className="px-6 py-3">Apellido</th>
+            <th scope="col" className="px-6 py-3">Género</th>
+            <th scope="col" className="px-6 py-3">Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.firebaseUID} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {user.firebaseUID}
+              </th>
+              <td className="px-6 py-4">{user.userType}</td>
+              <td className="px-6 py-4">{user.firstName}</td>
+              <td className="px-6 py-4">{user.lastName}</td>
+              <td className="px-6 py-4">{user.gender}</td>
+              <td className="px-6 py-4">{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default UsersTable;
---
+++ src/types/User.ts
@@ -0,0 +1,8 @@
+export interface User {
+    firebaseUID: string;
+    firstName: string;
+    lastName: string;
+    gender: string;
+    userType: string;
+    email: string;
+  }
